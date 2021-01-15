646b024be75f9d7b0b4ae9f90be73bafb2bbeb57
xXmantequillaXx <tashkinov.dig@students.dvfu.ru>
2021-01-15 11:15:20 +1000

    Добавьте файлы проекта.

---
+++ App_mobile_amonic/AmenitiesActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App_mobile_amonic
+{
+    [Activity(Label = "AmenitiesActivity")]
+    public class AmenitiesActivity : Activity
+    {
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Amenities);
+            
+
+            
+
+            // Create your application here
+        }
+    }
+}
